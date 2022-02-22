21238b012f72cac03eac58a8ed860d93ce228a0e
thuan753123 <khuuquocthuan@gmail.com>
2022-02-22 15:13:19 +0700

    Thuan update

--- public/js/app.js
+++ public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Dee\UniversitySystemG27\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Dee\UniversitySystemG27\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Document_CODE\System_project\Demo_system\UniversitySystemG27\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Document_CODE\System_project\Demo_system\UniversitySystemG27\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
