68f87410691a9fb2c276e4e583723a69eae20e77
datnguyen0901 <ntd8989@gmail.com>
2022-02-25 12:42:30 +0700

    update welcome  view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>University System G27</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -81,18 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    This is our group's website
+                </div>
+
+                <div class="title m-b-md">
+                     COMP1640 Enterprise Web Software Development
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://trello.com/b/RSRjLdbA/group-27-university-system-greenwich1640" target="_blank">Trello</a>
+                    <a href="https://github.com/datnguyen0901/UniversitySystemG27" target="_blank">GitHub</a>
                 </div>
             </div>
         </div>
