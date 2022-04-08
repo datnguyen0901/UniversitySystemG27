b41f839328f09dc569b34d80f97ea5e78e0f29c3
thuan753123 <khuuquocthuan@gmail.com>
2022-04-08 08:58:48 +0700

    update UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,89 +10,63 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+        
+        
+        
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+<body class="d-flex h-100 text-center text-white bg-dark">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <div>
+            <h3 class="float-md-start mb-0">Group G27</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                @auth
+                        <a class="nav-link active" aria-current="page" href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a class="nav-link"  href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a class="nav-link" href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
-                </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    This is our group's website
-                </div>
-
-                <div class="title m-b-md">
-                     COMP1640 Enterprise Web Software Development
-                </div>
-
-                <div class="links">
-                    <a href="https://trello.com/b/RSRjLdbA/group-27-university-system-greenwich1640" target="_blank">Trello</a>
-                    <a href="https://github.com/datnguyen0901/UniversitySystemG27" target="_blank">GitHub</a>
-                </div>
+            </nav>
             </div>
-        </div>
+        </header>
+
+        <main class="px-3">
+            <h1>Welcome group page.</h1>
+            <p class="lead">COMP1640 Enterprise Web Software Development</p>
+            <p class="lead">
+            <a  href="https://trello.com/b/RSRjLdbA/group-27-university-system-greenwich1640" target="_blank" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Trello</a>
+                </br>
+                </br>
+            <a href="https://github.com/datnguyen0901/UniversitySystemG27" target="_blank" class="btn btn-lg btn-secondary fw-bold border-white bg-white">GitHub</a>
+
+            </p>
+        </main>
+
+        <footer class="mt-auto text-white-50">
+            <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+        </footer>
+    </div>
     </body>
 </html>
